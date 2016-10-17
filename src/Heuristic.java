abedbb4e97b527204d90040ea5a75930cb4fd02e
lmish001 <lmish001@ucr.edu>
2016-10-17 16:31:13 -0700

    Initial working version. Still has warnings and needs testing

---
+++ src/Heuristic.java
@@ -0,0 +1,60 @@
+/**
+ * Created by eldgb on 17-Oct-16.
+ */
+public class Heuristic {
+
+
+    public static final String MISPLACEDTILE = "Misplaced tile";
+    public static final String MANHATTAN = "Manhattan distance";
+
+
+    public static int MisplacedTile(int[] state) {
+
+        int totalCost = 0;
+        for (int i = 0; i < state.length - 1; i++) {
+            if (i + 1 != state[i]) {
+                totalCost += 1;
+            }
+        }
+
+        return totalCost;
+
+    }
+
+
+    public static int ManhattanDistance(int[] state, int boardSize) {
+
+        int totalCost = 0;
+        int[][] board = new int[boardSize][boardSize];
+        int i, j;
+        int k = 0;
+        for (i = 0; i < boardSize; i++) {
+            for (j = 0; j < boardSize; j++) {
+                board[i][j] = state[k];
+                k++;
+            }
+        }
+
+        for (i = 0; i < boardSize; i++) {
+            for (j = 0; j < boardSize; j++) {
+
+                int value = board[i][j];
+                if (value != 0) {
+                    int posx = (value - 1) / boardSize;
+                    int posy = (value - 1) % boardSize;
+                    int disx = i - posx;
+                    int disy = j - posy;
+                    totalCost += Math.abs(disx) + Math.abs(disy);
+
+                }
+
+
+            }
+        }
+
+        return totalCost;
+
+    }
+
+
+}
---
+++ src/PrintResult.java
@@ -0,0 +1,29 @@
+/**
+ * Created by eldgb on 17-Oct-16.
+ */
+public class PrintResult {
+
+    public static void printCurrentState(int[] currentState, int cost, int boardSize) {
+
+        System.out.println("The best state to expand with a g(n) = 1 and h(n) = " + cost + " is…");
+
+        for (int i = 0; i < boardSize; i++) {
+            for (int j = 0; j < boardSize; j++) {
+                System.out.print(currentState[j + (i * boardSize)] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+    }
+
+    public static void printResult(int nodes, int queue, int depth) {
+
+        System.out.println("To solve this problem the search algorithm expanded a total of " + nodes + " nodes.");
+        System.out.println("The maximum number of nodes in the queue at any one time was " + queue);
+        System.out.println("The depth of the goal node was " + depth);
+
+    }
+
+
+}
